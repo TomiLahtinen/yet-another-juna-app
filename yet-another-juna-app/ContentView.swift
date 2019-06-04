31a6c78902b99d9e759a41289f15d1f8bdcdac5f
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2019-06-04 10:15:55 +0300

    Initial Commit

---
+++ yet-another-juna-app/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  yet-another-juna-app
+//
+//  Created by Tomi Lahtinen on 04/06/2019.
+//  Copyright © 2019 Tomi Lahtinen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
